ee206b36ef7fa398c49920ac5a4268a19f518327
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-29 17:18:00 +0200

    aggiunto dati recuperati

--- Program.cs
+++ Program.cs
@@ -4,11 +4,28 @@ using System.Data.SqlClient;
 
 string stringaDiConnessione = "Data Source=localhost;Initial Catalog=biblioteca-db;Integrated Security=True";
 
+Library library = new Library();
+
+Book firstBook = new Book(123456789, "L'arte della guerra");
+firstBook.pages = 200;
+
+Movie firstMovie = new Movie(123456789, "Star Wars L'impero colpisce ancora");
+firstMovie.minute = 150;
+
+Console.WriteLine(firstBook.title);
+Console.WriteLine(firstMovie.title);
+
+User userRegistered = new User("Mario", "Rossi", "mail@mail.com", "lamiapassword123", 123456789);
+User userAnonimous = new User("null", "null", "null", "null", 123456789);
+
+library.newUser(userRegistered);
+
 SqlConnection connessioneSql = new SqlConnection(stringaDiConnessione);
 
 try
 {
     connessioneSql.Open();
+
 }
 catch (Exception ex)
 {
@@ -21,21 +38,7 @@ finally
 
 //using System.Runtime.InteropServices;
 
-//Library library = new Library();
 
-//Book firstBook = new Book(123456789, "L'arte della guerra");
-//firstBook.pages = 200;
-
-//Movie firstMovie = new Movie(123456789, "Star Wars L'impero colpisce ancora");
-//firstMovie.minute = 150;
-
-//Console.WriteLine(firstBook.title);
-//Console.WriteLine(firstMovie.title);
-
-//User userRegistered = new User("Mario", "Rossi", "mail@mail.com", "lamiapassword123", 123456789);
-//User userAnonimous = new User("null", "null", "null", "null", 123456789);
-
-//library.newUser(userRegistered);
 
 //List<Document> risultati = library.searchElements("guerra");
 //Console.WriteLine(risultati[0].title);
